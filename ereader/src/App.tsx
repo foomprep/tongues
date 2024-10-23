d03de69545a57dbf558eda4246cae32c6da84661
George Balch <gbalch714@gmail.com>
2024-10-22 18:01:27 -0700

    Render book to window

--- ereader/src/App.tsx
+++ ereader/src/App.tsx
@@ -1,23 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import ePub from 'epubjs'
 
 function App() {
+  const [rendition, setRendition] = useState(null)
+
   const handleFileChange = (event) => {
     const file = event.target.files[0]
     if (file) {
       const reader = new FileReader()
       reader.onload = (e) => {
         const book = ePub(e.target.result)
-        console.log('New book created:', book)
+        const newRendition = book.renderTo('viewer', {
+          width: '100%',
+          height: '100%'
+        })
+        setRendition(newRendition)
+        newRendition.display()
       }
       reader.readAsArrayBuffer(file)
     }
   }
 
+  useEffect(() => {
+    return () => {
+      if (rendition) {
+        rendition.destroy()
+      }
+    }
+  }, [rendition])
+
   return (
     <>
       <input type="file" onChange={handleFileChange} accept=".epub" />
+      <div id="viewer" style={{ width: '100%', height: '80vh' }}></div>
     </>
   )
 }
